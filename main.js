3a776f7469c8b92ca029d04a42f4a077f145a69d
saswatabasu23 <saswatabasu23@gmail.com>
2021-09-03 10:20:24 +0530

    Comments added

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* Created by APPLE DEVELOPERS GROUP*/
+
+/* JSON Object*/
 var userData = {
     name: "",
     age: "",
@@ -5,6 +8,9 @@ var userData = {
     email: "",
     score: 0
 };
+
+
+/* Store and Calculate the score */
 class questionHandler {
     static questions = [];
     static questionNumber = 0;
@@ -30,15 +36,17 @@ class questionHandler {
     }
     static getFinalScore() {
         var s = 0;
-        for(var i = 0; i < this.questions.length; i++) {
-            if(this.questions[i].correctOption === this.answerSelected[i]) {
-                s+=1;
+        for (var i = 0; i < this.questions.length; i++) {
+            if (this.questions[i].correctOption === this.answerSelected[i]) {
+                s += 1;
             }
         }
         return s;
     }
 }
 
+
+/* API Call */
 const api_url = "https://task-3-api.herokuapp.com/questions";
 
 async function getapi() {
@@ -51,14 +59,16 @@ async function getapi() {
     });
 }
 
-window.addEventListener("DOMContentLoaded", async function() {
-    if(localStorage.getItem("userData") !== null) {
+
+
+window.addEventListener("DOMContentLoaded", async function () {
+    if (localStorage.getItem("userData") !== null) {
         userData = JSON.parse(localStorage.getItem("userData"));
         await renderScorePage();
     } else {
         await renderHomePage();
     }
-        
+
 }, false);
 
 async function fetchHtmlAsText(url) {
@@ -96,13 +106,13 @@ async function formSubmit(event) {
 async function beginQuiz() {
     var response = await getapi();
     // console.log(response);
-    if(response !== false) {
+    if (response !== false) {
         questionHandler.setQuestions(response);
         questionHandler.reset();
         document.querySelector('body').innerHTML = await fetchHtmlAsText("./quiz.html");
         renderQuestion();
         countdown(10, 0);
-    }  
+    }
 }
 
 async function renderQuestion() {
@@ -113,20 +123,20 @@ async function renderQuestion() {
     document.getElementById('optionC').innerHTML = questionHandler.getQuestion().optionC;
     document.getElementById('optionD').innerHTML = questionHandler.getQuestion().optionD;
     var radio = document.getElementsByName("options");
-    if(questionHandler.getAnswerSelected()) {
+    if (questionHandler.getAnswerSelected()) {
         radio[questionHandler.getSelectedAnswerIndex()].checked = true;
     } else {
-        for(var i=0;i<radio.length;i++)
+        for (var i = 0; i < radio.length; i++)
             radio[i].checked = false;
     }
     // console.log("Created questions!");
-    if(questionHandler.questionNumber === 9) {
+    if (questionHandler.questionNumber === 9) {
         document.getElementById('next-button').innerHTML = 'Submit';
     }
     else {
         document.getElementById('next-button').innerHTML = 'Next';
     }
-    if(questionHandler.questionNumber === 0) {
+    if (questionHandler.questionNumber === 0) {
         document.getElementById('previous-button').disabled = true;
     }
     else {
@@ -136,22 +146,22 @@ async function renderQuestion() {
 
 async function radioClicked() {
     var radio = document.getElementsByName("options");
-    for(var i=0;i<radio.length;i++) {
-        if(radio[i].checked == true) {
+    for (var i = 0; i < radio.length; i++) {
+        if (radio[i].checked == true) {
             questionHandler.setAnswerSelected(String.fromCharCode(65 + i));
         }
     }
 }
 
 async function nextQuestion() {
-    if(questionHandler.questionNumber == 9) {
+    if (questionHandler.questionNumber == 9) {
         quizOnSubmit();
     }
     else {
         questionHandler.questionNumber += 1;
         renderQuestion();
     }
-    
+
 }
 
 async function previousQuestion() {
@@ -169,7 +179,7 @@ async function quizOnSubmit() {
 
 async function updateLocalStorage() {
     let oldUserData = JSON.parse(localStorage.getItem("userData"));
-    if(userData.score > oldUserData.score) {
+    if (userData.score > oldUserData.score) {
         localStorage.setItem("userData", JSON.stringify(userData));
     }
 }
@@ -179,31 +189,28 @@ async function renderScorePage() {
     document.getElementById('score').innerHTML = userData.score.toString();
 }
 
-function countdown(minutes, seconds)
-{
+function countdown(minutes, seconds) {
     var element = document.getElementById('time');
     var endTime, hours, mins, msLeft, time;
 
-    function twoDigits(n)
-    {
+    function twoDigits(n) {
         return (n <= 9 ? "0" + n : n);
     }
 
-    function updateTimer()
-    {
+    function updateTimer() {
         msLeft = endTime - (+new Date);
-        if ( msLeft < 1000 ) {
+        if (msLeft < 1000) {
             quizOnSubmit();
         } else {
-            time = new Date( msLeft );
+            time = new Date(msLeft);
             hours = time.getUTCHours();
             mins = time.getUTCMinutes();
-            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
-            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+            element.innerHTML = (hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(time.getUTCSeconds());
+            setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
         }
     }
 
     element = document.getElementById('time');
-    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    endTime = (+new Date) + 1000 * (60 * minutes + seconds) + 500;
     updateTimer();
 }
